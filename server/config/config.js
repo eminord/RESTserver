594a6a49fd86787b33b43b038a2caa17bf16dcca
Esteban Gabriel Miño <emino@sesa-select.com.ar>
2019-02-07 10:38:51 -0300

    paso url de mlab a variable de heroku

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let url_db;
 if (process.env.NODE_ENV === 'DTA') {
     url_db = 'mongodb://localhost:27017/cafe';
 } else {
-    url_db = 'mongodb://node-randstad:node-randstad1.@ds121475.mlab.com:21475/cafe-node-emino';
+    url_db = process.env.REMOTE_DB;
 }
 
 process.env.URL_DB = url_db;
